29b8e9cc6c55dd4fa601af936bb5831416584574
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-04-13 12:42:03 +0200

    full CRUD

--- FilmDB/Controllers/FilmController.cs
+++ FilmDB/Controllers/FilmController.cs
@@ -1,27 +1,63 @@
 ﻿using FilmDB.Data;
 using FilmDB.Models;
+using FilmDB.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmDB.Controllers
 {
     public class FilmController : Controller
     {
-        private FilmContext _context;
-        public FilmController(FilmContext context) 
+        private FilmManager _manager;
+        public FilmController(FilmManager manager) 
         { 
-            _context = context;
+            _manager = manager;
         }
 
         public IActionResult Index()
         {
-            var film = new FilmModel();
-            film.ID = 0;
-            film.Title = "Titanic";
-            film.Year = "2000";
-            _context.Films.Add(film);
-            _context.SaveChanges();
+            var films = _manager.GetFilms();
+            return View(films);
+        }
 
+        [HttpGet]
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FilmModel film)
+        {
+            _manager.AddFilm(film);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var filmToRemove = _manager.GetFilm(id);
+            return View(filmToRemove);
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            _manager.RemoveFilm(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var filmToEdit = _manager.GetFilm(id);
+            return View(filmToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(FilmModel film)
+        {
+            _manager.UpdateFilm(film);
+            return RedirectToAction("Index");
         }
     }
 }
--- FilmDB/Program.cs
+++ FilmDB/Program.cs
@@ -1,4 +1,5 @@
 using FilmDB.Data;
+using FilmDB.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<FilmContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<FilmManager>();
 
 var app = builder.Build();
 
